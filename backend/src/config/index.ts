5a7986a797d7efcca5f946ea22f034e1fc90b10e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-12-15 16:40:58 +0300

    feat: add important commenting

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,12 +3,15 @@ import "dotenv/config";
 
 export const config = {
   port: process.env.PORT || 3000,
+  // file storage paths
   paths: {
     // navigate to the root and then into uploads/processed
     uploads: path.join(__dirname, "../../uploads"),
     processed: path.join(__dirname, "../../processed"),
   },
+  // cors setup
   cors: {
+    // Allow origin from any origin
     origin: "*",
   },
 };
